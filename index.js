741c332d3dc35ba1b433aabd3ab6be8d602e1dd9
Tomas Hubelbauer <tomas@hubelbauer.net>
2019-06-13 09:56:35 +0200

    Print event values to the body and stop events

---
+++ index.js
@@ -0,0 +1,24 @@
+window.addEventListener('load', () => {
+  /* Note that `document.body` doesn't span the whole viewport by default but `document` does */
+
+  document.addEventListener('mousemove', event => {
+    document.getElementById('mouseDiv').textContent = `${event.clientX} (${event.movementX}) ✕ ${event.clientY} (${event.movementY}) - ${event.buttons}`;
+    event.preventDefault();
+    event.stopPropagation();
+    return false;
+  });
+
+  document.addEventListener('pointermove', event => {
+    document.getElementById('pointerDiv').textContent = `${event.clientX} (${event.movementX}) ✕ ${event.clientY} (${event.movementY}) - ${event.buttons}`;
+    event.preventDefault();
+    event.stopPropagation();
+    return false;
+  });
+
+  document.addEventListener('touchmove', event => {
+    document.getElementById('touchDiv').textContent = [...event.touches].map(t => `${t.clientX.toFixed(0)} (${t.radiusX.toFixed(0)}) ✕ ${t.clientY.toFixed(0)} (${t.radiusY.toFixed(0)})`);
+    event.preventDefault();
+    event.stopPropagation();
+    return false;
+  });
+});
