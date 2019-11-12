9866407dffd4ff0b6fde2041f051a54811a0e90a
Tomas Hubelbauer <tomas@hubelbauer.net>
2019-11-12 20:15:49 +0100

    Use custom renderers for complex values

--- index.js
+++ index.js
@@ -1,54 +1,73 @@
 const events = {
-  MouseEvent: [
-    'altKey',
-    'button',
-    'buttons',
-    'clientX',
-    'clientY',
-    'ctrlKey',
-    'metaKey',
-    'movementX',
-    'movementY',
-    'offsetX',
-    'offsetY',
-    'pageX',
-    'pageY',
-    'region',
-    'relatedTarget',
-    'screenX',
-    'screenY',
-    'shiftKey',
-    'which',
-    // 'mozPressure', -> PointerEvent.pressure
-    'mozInputSource',
-    'webkitForce',
-    'x',
-    'y'
-  ],
-  TouchEvent: [
-    'altKey',
-    'changedTouches',
-    'ctrlKey',
-    'metaKey',
-    'shiftKey',
-    'targetTouches',
-    'touches',
-    'rotation',
-    'scale'
-  ],
-  PointerEvent: [
-    'pointerId',
-    'width',
-    'height',
-    'pressure',
-    'tangentialPressure',
-    'tiltX',
-    'tiltY',
-    'twist',
-    'pointerType',
-    'isPrimary'
-  ]
+  mouse: {
+    altKey: e => e.altKey ? 'yes' : 'no',
+    button: e => ['left', 'middle', 'right', 'back', 'forward'][e.button] + ' button',
+    buttons: e => ['no', 'left', 'right', '?', 'middle', '?', '?', '?', 'back', '?', '?', '?', '?', '?', '?', '?', 'forward'][e.buttons] + ' button',
+    clientX: e => e.clientX + 'px from viewport edge (excludes scroll)',
+    clientY: e => e.clientY + 'px from viewport edge (excludes scroll)',
+    ctrlKey: e => e.ctrlKey ? 'yes' : 'no',
+    metaKey: e => e.metaKey ? 'yes' : 'no',
+    movementX: e => e.movementX + 'px',
+    movementY: e => e.movementY + 'px',
+    offsetX: e => e.offsetX + 'px from self edge',
+    offsetY: e => e.offsetY + 'px from self edge',
+    pageX: e => e.pageX + 'px from document edge (includes scroll)',
+    pageY: e => e.pageY + 'px from document edge (includes scroll)',
+    region: e => e.region ? 'a canvas region' : 'no canvas region',
+    relatedTarget: e => e.relatedTarget ? e.relatedTarget.tagName + ' element' : 'no element',
+    screenX: e => e.screenX + 'px from monitor edge',
+    screenY: e => e.screenY + 'px from monitor edge',
+    shiftKey: e => e.shiftKey ? 'yes' : 'no',
+    which: e => ['no', 'left', 'middle', 'right'][e.which] + ' button',
+    // mozPressure = PointerEvent.pressure
+    mozInputSource: e => ['unknown', 'mouse', 'pen', 'eraser', 'cursor', 'touch', 'keyboard'][e.mozInputSource],
+    webkitForce: e => e.webkitForce ? e.webkitForce + ' (force touch)' : 'no force touch',
+    x: e => e.x + 'px (= clientX)',
+    y: e => e.y + 'px (= clientY)'
+  },
+  touch: {
+    altKey: e => e.altKey ? 'yes' : 'no',
+    changedTouches: e => e.changedTouches ? renderTouchList(e.changedTouches) : 'none',
+    ctrlKey: e => e.ctrlKey ? 'yes' : 'no',
+    metaKey: e => e.metaKey ? 'yes' : 'no',
+    shiftKey: e => e.shiftKey ? 'yes' : 'no',
+    targetTouches: e => e.targetTouches ? renderTouchList(e.targetTouches) : 'none',
+    touches: e => e.touches ? renderTouchList(e.touches) : 'none',
+    rotation: e => e.rotation ? e.rotation + 'deg' : 'none (2+ touches)',
+    scale: e => e.scale ? e.scale : 'none (2+ touches)'
+  },
+  pointer: {
+    pointerId: e => e.pointerId,
+    width: e => e.width + 'px',
+    height: e => e.height + 'px',
+    pressure: e => '~' + Math.round(e.pressure * 100) + '%',
+    tangentialPressure: e => '~' + Math.round(e.tangentialPressure * 100) + '%',
+    tiltX: e => e.tiltX ? e.tiltX + 'deg' : 'none',
+    tiltY: e => e.tiltY ? e.tiltY + 'deg' : 'none',
+    twist: e => e.twist ? e.twist + 'deg' : 'none',
+    pointerType: e => e.pointerType || 'none',
+    isPrimary: e => e.isPrimary ? 'yes' : 'no'
+  }
 };
+
+function renderTouchList(touches) {
+  let result = touches.length + ' touches<br />';
+  for (const touch of touches) {
+    result += `screen: ${t.screenX}, ${t.screenY} |`;
+    result += `client: ${t.clientX}, ${t.clientY} |`;
+    result += `page: ${t.pageX}, ${t.pageY} |`;
+    result += `target: ${t.target ? 'some' : 'none'}`;
+    result += '<br />';
+  }
+
+  return result;
+}
+
+const variants = {
+  mouse: ['down', 'move', 'up', 'enter', 'leave'],
+  touch: ['down', 'move', 'up'],
+  pointer: ['down', 'move', 'up']
+}
 
 window.addEventListener('load', () => {
   for (const event in events) {
@@ -58,14 +77,19 @@ window.addEventListener('load', () => {
     eventDetails.append(eventSummary);
     const eventA = document.createElement('a');
     eventA.textContent = event;
-    eventA.href = 'https://developer.mozilla.org/en-US/docs/Web/API/' + event;
+    eventA.href = `https://developer.mozilla.org/en-US/docs/Web/API/${event}Event`;
+    eventA.target = 'blank';
     eventSummary.append(eventA);
     const eventTable = document.createElement('table');
     const props = events[event];
-    for (const prop of props) {
+    for (const prop in props) {
       const propTr = document.createElement('tr');
       const nameTh = document.createElement('th');
-      nameTh.textContent = prop;
+      const nameA = document.createElement('a');
+      nameA.textContent = prop;
+      nameA.href = `https://developer.mozilla.org/en-US/docs/Web/API/${event}Event/${prop}`;
+      nameA.target = 'blank';
+      nameTh.append(nameA);
       propTr.append(nameTh);
       const valueTd = document.createElement('td');
       valueTd.id = event + '.' + prop;
@@ -76,11 +100,12 @@ window.addEventListener('load', () => {
     eventDetails.append(eventTable);
     document.body.append(eventDetails);
 
-    for (const variant of ['down', 'move', 'up']) {
-      window.addEventListener(event.slice(0, -'event'.length).toLowerCase() + variant, e => {
-        for (const prop of props) {
-          const value = e[prop];
-          document.getElementById(event + '.' + prop).textContent = value;
+
+    for (const variant of variants[event]) {
+      window.addEventListener(event + variant, e => {
+        for (const prop in props) {
+          const value = props[prop](e);
+          document.getElementById(event + '.' + prop).innerHTML = value;
         }
       }, { passive: true });
     }
